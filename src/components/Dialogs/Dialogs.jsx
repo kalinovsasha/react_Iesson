0327adb8c67d5024d8d44d1992315c900bca4316
Alexander <kalinovsasha@yandex.ru>
2020-05-17 21:59:55 +0300

    object,map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import "./Dialogs.css";
 import { Link } from 'react-router-dom';
 
+
+
 function Dialogs(props) {
+    let friends =[{id:"Dialogs/1",name:"Вася"},{id:"Dialogs/2",name:"Serje"},{id:"Dialogs/3",name:"Петя"},{id:"Dialogs/4",name:"Коля"}];
+    let friend = friends.map((item)=><li><Link to={item.id}> {item.name}</Link></li>);
+   
     return(
         <div className="Dialogs_Main">
             <div className="Dialogs_Interlocutor">
             <section className="Dialogs_MesText">Messages</section>
                 <ul className="Dialog_FriendList">
-                    <li><Link to="/Dialogs/1">Вася</Link></li>
-                    <li><Link to="">Петя</Link></li>
-                    <li><Link to="">Коля</Link></li>
+                    {friend}
+                    <li><Link to="/Dialogs">Админ</Link></li>
                 </ul>
             </div>
             <div className="Dialog_MessagesList">
