acc3aab95b754209f6071bae45529ef16cfc41f4
Alexander <kalinovsasha@yandex.ru>
2020-05-26 20:31:14 +0300

    v2

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Render} from './render'
 import store from './redux/state'
 
 
 
-
-//Render(store.getState(),store.addPost,store.newPost);
-Render(store);
-
-
-
-serviceWorker.unregister();
-
-
-/*
-function Render(state,addPost,newPost) {
+function Render(store) {
+  let state=store.getState();
     ReactDOM.render(
         <React.StrictMode>
-          <App profInfo={state.profInfo} posts={state.posts} bufPost={state.bufPost}  dialogsFriendList={state.dialogsFriendList} addPost={addPost} newPost={newPost} /> 
+          <App profInfo={state.profInfo} posts={state.posts} bufPost={state.bufPost}  dialogsFriendList={state.dialogsFriendList} addPost={store.addPost} newPost={store.newPost} /> 
         </React.StrictMode>,
         document.getElementById('root')
       );
 }
-*/
+
+
+//Render
+Render(store);
+//Передаем функцию Render
+store.subscribeRender(Render);
+
+serviceWorker.unregister();
+
+
+
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,4 @@
-import {Render} from '../render'
+
 
 
  let store={
@@ -8,6 +8,14 @@ import {Render} from '../render'
     bufPost:{mes:"",ava:"https://sun9-56.userapi.com/c836234/v836234894/5aedd/--93nsktQsI.jpg"},
     dialogsFriendList:[{id:"Dialogs/1",name:"Вася"},{id:"Dialogs/2",name:"Serje"},{id:"Dialogs/3",name:"Петя"},{id:"Dialogs/4",name:"Коля"},{id:"Dialogs/5",name:"Volodya"}],
 },
+  //Функция для отрисовки
+  render(store){
+    console.log("null");
+  },
+  //Подписываемся на функцию отрисовки
+  subscribeRender(observer){
+    this.render=observer;
+  },
 
   getState(){
     return this._state;
@@ -15,42 +23,16 @@ import {Render} from '../render'
 
   //Сохрарнение набранного текста в newPost
   newPost (txt){
-    console.log(this);
     this._state.bufPost.mes=txt;
-    Render(store);
+    this.render(store);
   },
   
   //Добавление поста на стену, текст поста берется из newPost
   addPost (){
     this._state.posts.push(this._state.bufPost);
-    Render(store);
+    this.render(store);
   },
 }
 
 export default store;
 
-
-//Бекап кода до рефакторинга
-
-/* let state ={
-        profInfo : {birthday:"03.01.1994",country:"Belarus",educate:"GRSU grodno", website:"vk.com/kalinovsasha", ava:"https://sun9-56.userapi.com/c836234/v836234894/5aedd/--93nsktQsI.jpg"},
-        posts:[{mes:"Hello", ava:"https://yt3.ggpht.com/a/AATXAJwcRz4MbWjSrTeWdpzfyVsPC97gRqnc_YC-qQ=s900-c-k-c0xffffffff-no-rj-mo"},{mes:"My name is sasha"},{mes:"Как дела?"}],
-        bufPost:{mes:"",ava:"https://sun9-56.userapi.com/c836234/v836234894/5aedd/--93nsktQsI.jpg"},
-     
-      dialogsFriendList:[{id:"Dialogs/1",name:"Вася"},{id:"Dialogs/2",name:"Serje"},{id:"Dialogs/3",name:"Петя"},{id:"Dialogs/4",name:"Коля"},{id:"Dialogs/5",name:"Volodya"}],
-};
-//Сохрарнение набранного текста в newPost
-export function newPost(txt){
-  state.bufPost.mes=txt;
-  Render(state,addPost,newPost);
-}
-
-//Добавление поста на стену, текст поста берется из newPost
-export function addPost (){
-    state.posts.push(state.bufPost);
-    Render(state,addPost,newPost);
-}
-
-
-export default state;
-*/
